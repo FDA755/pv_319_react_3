5de886dda38beaf77f826a2073ae05049703cd3f
Legion <bobyzz@mail.ru>
2025-09-05 16:26:02 +0300

    State day 2

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,32 @@ import './App.css';
 import Header from './header/Header.js';
 import Hello from './hello/Hello.js';
 import Length from './length/Length.js';
+import Form from './form/Form.js';
+import Range from './range/Range.js';
+import Posts from './posts/Posts.js';
+import Post from './post/Post.js';
 
 class App extends React.Component {
+  state = 
+  {
+    posts:
+    [
+      {id:"1", title:"Процедурное программирования на языке C++", content:"Изучаются базовые конструкции языка С++"},
+      {id:"2", title:"Объектно-ориентированное программирования на языке C++", content:"Изучаются базовые концепции ООП"},
+      {id:"3", title:"DataCOntainers", content:"Изучаются основные структуры данных и контейнеры, основанные на них"}
+    ]
+  }
    render() {
     return (
       <div className="App">
         <Header />
-        <Hello />
+        {/* <Hello />
         <Length />
+        <Form />
+        <Range /> */}
+        {typeof(this.state.posts)}
+        <Posts posts={this.state.posts} />
+        {/* <Post title = "Title" content = "Content"/> */}
       </div>
     );
   }
